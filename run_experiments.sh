416560f0fab61196d50b6d5920a63855a40edc70
Ignacio Requeno <ppnacho85@gmail.com>
2024-08-14 18:19:19 +0200

    Pictures from the experiments for a complete year

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-for attack in "avg" "min_avg" "max_avg" "fdi50" "fdi150" "rsa01_08" "rsa2_5" "swap" "normal"
+# List of attacks: "avg" "min_avg" "max_avg" "fdi50" "fdi150" "rsa01_08" "rsa2_5" "swap" "normal"
+for attack in "min_avg" "max_avg" "fdi50" "fdi150" "swap" "normal"
 do
-  # python ./experiments.py $attack ./day_0_360_user_1143/$attack/days_*
-	python ./experiments.py $attack ./day_0_50_user_1143/$attack/days_*
+  python ./experiments.py $attack ./day_0_360_user_1143/$attack/days_*
+	# python ./experiments.py $attack ./day_0_50_user_1143/$attack/days_*
 done
 
 for attack in "rsa01_08" "rsa2_5"
 do
-  # python ./experiments_for_rsa.py $attack ./day_0_360_user_1143/$attack/days_*
-	python ./experiments_for_rsa.py $attack ./day_0_50_user_1143/$attack/days_*
+  python ./experiments_for_rsa.py $attack ./day_0_360_user_1143/$attack/days_*
+	# python ./experiments_for_rsa.py $attack ./day_0_50_user_1143/$attack/days_*
 done
