8f17c1fd2c63382e6789750b3eb9875396bb084e
Ignacio Requeno <ppnacho85@gmail.com>
2024-09-24 10:50:37 +0200

    Removing days_0_50 so that we only keep days_0_360

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,11 +3,9 @@
 for attack in "avg" "min_avg" "max_avg" "fdi50" "fdi150" "swap" "normal"
 do
   python ./experiments.py $attack ./day_0_360_user_1143/$attack/days_*
-	# python ./experiments.py $attack ./day_0_50_user_1143/$attack/days_*
 done
 
 for attack in "rsa01_08" "rsa2_5"
 do
   python ./experiments_for_rsa.py $attack ./day_0_360_user_1143/$attack/days_*
-	# python ./experiments_for_rsa.py $attack ./day_0_50_user_1143/$attack/days_*
 done
